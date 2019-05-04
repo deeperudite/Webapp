7e5fe946e1a52d2f021aa30c570d8eebcad0b246
NithishB <nithish.mdh@gmail.com>
2019-05-04 17:27:10 +0530

    API_Updates

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,35 @@
+from flask_restful import Resource, Api, reqparse
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
 from model_predict import model
 from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+parser = reqparse.RequestParser()
 
 class ASAGGrader(Resource):
     def post(self):
+        parser.add_argument('quest',type=str)
+        parser.add_argument('ref_ans',type=str)
+        parser.add_argument('stu_ans',type=str)
+        parser.add_argument('model',type=str)
         q,ra,sa,mf = "","","",""
-        content = request.get_json(force=True)
-        try:
-            q = content['quest']
-            ra = content['ref_ans']
-            sa = content['stu_ans']
-            mf = content['model']
-        except:
-            pass
+        # content = request.get_json(force=True)
+        # try:
+        #     q = content['quest']
+        #     ra = content['ref_ans']
+        #     sa = content['stu_ans']
+        #     mf = content['model']
+        # except:
+        #     pass
+        args = parser.parse_args()
+        q = args['quest']
+        ra = args['ref_ans']
+        sa = args['stu_ans']
+        mf = args['model']
         grade = model(q,ra,sa,mf)
-        return {'quest':q,'ref_ans':ra,'stu_ans':sa,'mtype':mf,'grade':grade}
+        return {'quest':q,'ref_ans':ra,'stu_ans':sa,'model':mf,'grade':grade}
 
 api.add_resource(ASAGGrader, '/')
 
