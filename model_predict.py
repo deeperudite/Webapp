6518d2d97ee97d6d7f07564bc76377d85184dbe2
NithishB <nithish.mdh@gmail.com>
2019-05-04 23:15:59 +0530

    Check Random Input Model Work

--- model_predict.py
+++ model_predict.py
@@ -5,15 +5,16 @@ def model(q,ra,sa,mtype):
     #
     # # load the saved model and return prediction
     # pred = ""
-    # if mtype == "classifier":
-    #     loaded_model = joblib.load("classifier.sav")
-    #     pred = loaded_model.predict(inp_feat).ravel()[0]
-    #     if pred >= 0.5:
-    #         return "Correct"
-    #     else:
-    #         return "Incorrect"
-    # else:
-    #     loaded_model = joblib.load("regressor.sav")
-    #     pred = loaded_model.predict(inp_feat).ravel()
-    #     return str(pred)
+    inp_feat = np.random.rand(1,59)
+    if mtype == "classifier":
+        loaded_model = joblib.load("classifier.sav")
+        pred = loaded_model.predict(inp_feat).ravel()[0]
+        if pred >= 0.5:
+            return "Correct"
+        else:
+            return "Incorrect"
+    else:
+        loaded_model = joblib.load("regressor.sav")
+        pred = loaded_model.predict(inp_feat).ravel()
+        return str(pred)
     return "Correct"
